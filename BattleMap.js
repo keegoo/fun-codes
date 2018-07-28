9b92faeb36c8a81b0f1204b4b9a138a5ccbf6968
CY <onions.yang@gmail.com>
2018-07-29 01:07:18 +0800

    add get_an_untaken_position()

--- BattleMap.js
+++ BattleMap.js
@@ -5,18 +5,34 @@ const EMPTY_BLOCK = 0
 
 class BattleMap {
 
-  constructor(height, width) {
-    this.height = height
-    this.width = width
+  constructor(rows, columns) {
+    this.rows = rows
+    this.columns = columns
 
     // setting
     this._init_strength = 10
 
-    this.battleMap = this._create_map(height, width)
+    this.battleMap = this._create_map(rows, columns)
   }
 
   player_take_position(player, row, column) {
     this.battleMap = this._update_map(this.battleMap, row, column, player, this._init_strength)
+  }
+
+  get_an_untaken_position() {
+    const positions = this._calculate_untaken_positions(this.battleMap)
+    return positions[Math.floor(Math.random()*positions.length)]
+  }
+
+  render() {
+    this.battleMap.map( row => this._render_row(row) )
+  }
+
+  debug() {
+    this.battleMap.map( (row, index) => {
+      console.log(`ROW ${index}`)
+      console.log(JSON.stringify(row))
+    })
   }
 
   _update_map(battleMap, row, column, player, strength) {
@@ -25,24 +41,26 @@ class BattleMap {
     return Object.assign(battleMap, {row: new_row})
   }
 
-  _create_map(height, width) {
-    return Array.from(Array(this.height), (x, i) => {
-      return Array.from(Array(this.width), (x, i) => { 
+  _create_map(rows, columns) {
+    return Array.from(Array(this.rows), (x, i) => {
+      return Array.from(Array(this.columns), (x, i) => { 
         return {player: 0, strength: 0} 
       })
     })
   }
 
-  // render the whole map
-  render() {
-    this.battleMap.map( row => this._render_row(row) )
-  }
-
-  debug() {
-    // this.battleMap[row][column]
-    this.battleMap.map( row => {
-      console.log(row)
-    })
+  _calculate_untaken_positions(battleMap) {
+    const rows = battleMap.length
+    const columns = battleMap[0].length
+    let empty_blocks = []
+    for (let row = 0; row < rows; row ++) {
+      for(let column = 0; column < columns; column ++) {
+        if(battleMap[row][column].player == EMPTY_BLOCK) {
+          empty_blocks.push([row, column])
+        }
+      }
+    }
+    return empty_blocks
   }
 
   _render_row(row) {
