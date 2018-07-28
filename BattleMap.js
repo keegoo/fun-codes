4ffffea1685776fc72939ca4acc4760158d0d2aa
CY <onions.yang@gmail.com>
2018-07-29 00:02:27 +0800

    make map update as pure as possible; add update_map function

--- BattleMap.js
+++ BattleMap.js
@@ -12,18 +12,37 @@ class BattleMap {
     // setting
     this._init_strength = 10
 
-    this._init_map(height, width)
+    this.battleMap = this._create_map(height, width)
   }
 
-  _init_map(height, width) {
-    const dot = { player: 0, strength: 0 }
-    const row = Array.from(Array(this.width), (x, i) => dot)
-    this.battleMap = Array.from(Array(this.height), (x, i) => row)
+  player_take_position(player, row, column) {
+    this.battleMap = this._update_map(this.battleMap, row, column, player, this._init_strength)
+  }
+
+  _update_map(battleMap, row, column, player, strength) {
+    const dot = {player: player, strength: strength}
+    const new_row = Object.assign(battleMap[row], {[column]: dot})
+    return Object.assign(battleMap, {row: new_row})
+  }
+
+  _create_map(height, width) {
+    return Array.from(Array(this.height), (x, i) => {
+      return Array.from(Array(this.width), (x, i) => { 
+        return {player: 0, strength: 0} 
+      })
+    })
   }
 
   // render the whole map
   render() {
-    this.battleMap.map( row => this._render_row(row))
+    this.battleMap.map( row => this._render_row(row) )
+  }
+
+  debug() {
+    // this.battleMap[row][column]
+    this.battleMap.map( row => {
+      console.log(row)
+    })
   }
 
   _render_row(row) {
